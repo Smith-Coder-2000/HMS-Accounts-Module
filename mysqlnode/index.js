a6431a308cdaa5d164ec39d4db2ac9b3ad81e4c8
Smith-Coder-2000 <96162154+Smith-Coder-2000@users.noreply.github.com>
2022-02-18 10:19:00 +0530

    Add files via upload

---
+++ mysqlnode/index.js
@@ -0,0 +1,51 @@
+//https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
+
+const mysql=require('mysql');
+const express = require('express')
+const http = require('http');
+const path = require("path");
+var cors = require('cors')
+const app = express()
+const server = http.createServer(app);
+app.use(cors())
+
+
+var connection=mysql.createConnection(
+  {
+    host:'localhost',
+    user:'root',
+    password:'roottoor',
+    database:'hms_accounts'
+  
+  });
+
+
+
+
+connection.connect((err)=>{
+  if(!err)
+  console.log('mysql connected');
+  else
+  console.log(err);
+})
+
+
+app.get('/patient', (req, res) => {
+  connection.query('SELECT * FROM patient',(err,rows,fields)=>{
+    if (err) {
+      console.log("File read failed:", err);
+      return;
+    }
+    try{
+        console.log(rows)
+        res.json({patient : rows})
+        
+    }catch (err) {
+        console.log('error parsing JSON',err)
+    }
+    });
+});
+
+server.listen(3000, function(){
+  console.log("server is listening on port: 3000");
+});
